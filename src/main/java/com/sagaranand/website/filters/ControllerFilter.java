b43dba9eab94cdddcb65c057ed0716dfdea63573
Sagar Anand <sagar.anand015@gmail.com>
2016-11-10 10:37:25 +0530

    Added the no-cache and other headers in the filters for all the /app resources

---
+++ src/main/java/com/sagaranand/website/filters/ControllerFilter.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package com.sagaranand.website.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.SynthesizedAnnotation;
+
+/**
+ * @author sanand5
+ *
+ */
+public class ControllerFilter implements Filter {
+
+	private static final Logger logger = LoggerFactory.getLogger(ControllerFilter.class);
+
+	private FilterConfig filterConfig;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+	 * javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		try {
+
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpServletResponse res = (HttpServletResponse) response;
+
+			String servletPath = req.getServletPath();
+			System.out.println("Servlet path is: " + servletPath);
+			if (!startsWithStaticPath(servletPath.trim())) {
+				res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+				res.setHeader("Pragma", "no-cache");
+				res.setHeader("Expires", "0");
+				res.setHeader("X-Frame-Options", "DENY");
+			}
+			chain.doFilter(request, response);
+
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	// For css, scripts, images, htmls, rest - login not needed
+	private boolean startsWithStaticPath(String servletPath) {
+		if (servletPath.trim().startsWith("/libraries/")) {
+			return true;
+		}
+		return false;
+	}
+
+}
