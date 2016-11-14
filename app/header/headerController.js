cdb5e1f919c83b164039a67da305510c2eb0a280
Sagar Anand <sagar.anand015@gmail.com>
2016-11-14 13:01:40 +0530

    Added the configurable main.json file for global site things

--- app/header/headerController.js
+++ app/header/headerController.js
@@ -2,7 +2,35 @@ angular
 	.module('monitorApp')
 	.controller('headerController', headerController);
 
-	function headerController() {
-		console.log("Logging headerController");
-		var vm = this;
+headerController.$inject = ['$scope', 'dataFactory'];
+
+function headerController($scope, dataFactory) {
+	console.log("Logging headerController");
+	var vm = this;
+
+	vm.pageContent = {};
+	$scope.siteName = "";
+
+	vm.initHeader = function() {
+		vm.headerContentResp = dataFactory.getPageContents("main").then(function(response) {
+			console.log(response);
+			if(response.status == 200) {
+				$scope.siteName = response.data.siteName;
+			} else {
+				ngToast.create({
+					className: 'danger',
+					content: 'Could not Load the Page Contents. Please try again.'
+				});
+			}
+		}, function(response) {
+			ngToast.create({
+				className: 'danger',
+				content: 'Could not Load the Page Contents. Please try again.'
+			});
+		});
 	}
+
+	// load the contents of the page with this call to the initHome()
+	vm.initHeader();
+
+}
